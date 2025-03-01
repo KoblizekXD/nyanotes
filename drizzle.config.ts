df0fc6ae644cac1b63c811fe0bb7f2e24518dc07
KoblizekXD <janprokupek@centrum.cz>
2025-03-01 19:24:49 +0100

    Add shadcn base

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,11 @@
-import env from '@/lib/env';
-import 'dotenv/config';
-import { defineConfig } from 'drizzle-kit';
+import env from "@/lib/env";
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  out: './drizzle',
-  schema: './src/db/schema.ts',
-  dialect: 'postgresql',
+  out: "./drizzle",
+  schema: "./src/db/schema.ts",
+  dialect: "postgresql",
   dbCredentials: {
     url: env.DATABASE_URL,
   },

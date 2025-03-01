2a06d18486c3be630349e880852a11d10a31fc2d
KoblizekXD <janprokupek@centrum.cz>
2025-03-01 18:38:11 +0100

    More Drizzle configuration

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,8 +1,11 @@
+import { config } from "dotenv";
 import { z } from "zod";
+
+config({ path: ".env.local" });
 
 export const env = z.object({
   DATABASE_URL: z.string(),
-  NODE_ENV: z.string(),
+  NODE_ENV: z.optional(z.string().default(process.env.NODE_ENV)),
   BETTER_AUTH_SECRET: z.string(),
   BETTER_AUTH_URL: z.string(),
 });
